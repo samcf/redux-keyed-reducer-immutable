1aaacfc06e3dd953f64497034a797a44910e3583
Samuel Ferrell <samcferrell@gmail.com>
2018-12-14 21:05:45 -0600

    Initial commit.

---
+++ index.test.ts
@@ -0,0 +1,45 @@
+import Immutable from "immutable";
+import { Action, combineReducers, createStore } from "redux";
+import { bindKeyedActions } from "redux-keyed-reducer";
+import test from "tape";
+
+import { createKeyedReducer } from "./index";
+
+type ICounterState = number;
+
+enum CounterActions {
+    Increment = "Increment",
+    Decrement = "Decrement",
+}
+
+const counter = (state: ICounterState = 0, action: Action<CounterActions>) => {
+    switch (action.type) {
+        case CounterActions.Increment:
+            return state + 1;
+        case CounterActions.Decrement:
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+test("foo", (assert) => {
+    const reducer = createKeyedReducer(counter, "counter", { isKeyRequired: false });
+    const store = createStore(combineReducers({
+        counter: reducer,
+    }));
+
+    const actions = bindKeyedActions({
+        onDecrement: () => ({ type: CounterActions.Decrement }),
+        onIncrement: () => ({ type: CounterActions.Increment }),
+    }, { counter: "testKey" }, store.dispatch);
+
+    actions.onIncrement();
+
+    assert.ok(
+        store.getState().counter.equals(Immutable.Map({
+            default: 0,
+            testKey: 1,
+        })),
+    );
+});
---
+++ index.ts
@@ -0,0 +1,63 @@
+import Immutable from "immutable";
+import { Map } from "immutable";
+import { Action, AnyAction, Reducer } from "redux";
+
+interface IKeyedReducerOptions {
+    isKeyRequired?: boolean;
+}
+
+interface IStoreKeys {
+    [key: string]: string;
+}
+
+type IKeyedState<S> = Map<string, S>;
+
+// SENTINEL_ACTION is a Redux action that is unlikely to be "switched" over in
+// the given reducer. This is useful for deriving the initial state of the
+// reducer.
+const SENTINEL_ACTION: Action<"@@ReduxKeyedReducerImmutable"> = {
+    type: "@@ReduxKeyedReducerImmutable",
+};
+
+// DEFAULT_INSTANCE_KEY is the name of the key in the keyed reducer state whose
+// value will always be the initial state for the given reducer function. This
+// is derived by running the given reducer with a fake action that is unlikely
+// to be "switched" over.
+const DEFAULT_INSTANCE_KEY = "default";
+
+// getStoreKeys returns an object of store keys found decorated on the given
+// action. Returns an empty object if no store keys are found.
+function getStoreKeys(action: AnyAction): IStoreKeys {
+    if (!("meta" in action)) {
+        return {};
+    }
+
+    if (!("storeKeys" in action.meta)) {
+        return {};
+    }
+
+    return action.meta.storeKeys;
+}
+
+// createKeyedReducer returns a new Redux reducer. This reducer's state is
+// an Immutable.Map containing one or more instances of the given reducer's
+// state.
+export function createKeyedReducer<S, A extends Action = AnyAction>(
+    reducer: Reducer<S, A>,
+    storeKey: string,
+    options: IKeyedReducerOptions = {},
+) {
+    const initialState = reducer((void 0), SENTINEL_ACTION as A);
+    return (state: IKeyedState<S> = Immutable.Map(), action: A) => {
+        state = state.set(DEFAULT_INSTANCE_KEY, initialState);
+        if (storeKey in getStoreKeys(action)) {
+            return state.set(storeKey, reducer(state.get(storeKey), action));
+        }
+
+        if (options.isKeyRequired) {
+            return state;
+        }
+
+        return state.map((prevState) => reducer(prevState, action));
+    };
+}
